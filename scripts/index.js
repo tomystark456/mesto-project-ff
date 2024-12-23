5539c7c9749a3e4edd968f789cbda5d8fa853f85
tomystark456 <tomystark456@gmail.com>
2024-12-23 17:40:08 +0500

    added cards and delete-fallback

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
+const cardsPlace = document.querySelector('.places__list');
+const deleteSelf = e => e.target.closest('.places__item').remove();
 
-// @todo: DOM узлы
+const createCard = function (cardData, deleteCallback) {
 
-// @todo: Функция создания карточки
+  const template = document.querySelector('#card-template').content;
+  const card = template.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция удаления карточки
+  const cardImage = card.querySelector('.card__image');
+  const cardDescription = card.querySelector('.card__description').querySelector('.card__title');
+  const cardRemoveButton = card.querySelector('.card__delete-button');
 
-// @todo: Вывести карточки на страницу
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardDescription.textContent = cardData.name;
+
+  cardRemoveButton.onclick = e => deleteCallback(e);
+
+  return card;
+}
+
+initialCards.forEach(e => cardsPlace.append(createCard(e, deleteSelf)));
